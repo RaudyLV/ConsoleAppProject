5014c81fa3f22805f18810e943d3c03814f2994f
Raudy <Raudy206@gmail.com>
2025-04-16 14:46:08 -0400

    Modificando program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿global using System;
+
+public class ProgramaPrueba {
+    public static void main(String[] args) {
+        Console.WriteLine("Hola desde nuevo commit");
+    }
+}
