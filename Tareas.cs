c06f727c228327e50f9efb7d9d7dc38864050e15
Raudy <Raudy206@gmail.com>
2025-04-16 22:11:09 -0400

    Moviendo funcionalidades y mejoras para las tareas

--- Tareas.cs
+++ Tareas.cs
@@ -1,12 +1,53 @@
 using System;
-
+using System.Linq;
 public class Tarea {
-    public int Id { get; set; }
+    private int Id { get; set; }
     public string? Description { get; set; }
-    public bool IsCompleted { get; set; }
+    private bool IsCompleted { get; set; }
+    private List<Tarea> Tareas { get; set;} = new List<Tarea>();
+    private int Contador { get; set; } = 0;
 
     public override string ToString()
     {
         return $"Tarea: {Id}, {Description}. Estado: {(IsCompleted ? "Completada" : "Pendiente")}";
+    }
+
+      public  void AgregarTarea() {
+        Console.Clear();
+        Console.Write("Digite la descripcion de la tarea: ");
+        string descripcion = Console.ReadLine() ?? string.Empty;
+
+        Tareas.Add(new Tarea{Id = ++Contador, Description = descripcion, IsCompleted = false });
+
+         Console.Write("Pulse enter para continuar...");
+            Console.ReadKey();
+    }
+
+    public void ActualizarTarea(){
+        Console.Clear();
+        Console.Write("Digite el ID de la tarea ");
+        int id;
+        while(!int.TryParse(Console.ReadLine(), out id) ){
+            Console.WriteLine("Ingrese un numero valido: ");
+        }
+
+        if(!Tareas.Any(t => t.Id == id)){
+            Console.WriteLine("Tarea no encontrada!");
+        }else{
+
+            Tareas[id-1].IsCompleted = true;
+        }
+        
+         Console.Write("Pulse enter para continuar...");
+            Console.ReadKey();
+    }
+
+    public void VerTareas() {
+        Console.Clear();
+        foreach(var tarea in Tareas){
+            Console.WriteLine( tarea.ToString());
+        }
+         Console.Write("Pulse enter para continuar...");
+            Console.ReadKey();
     }
 }
